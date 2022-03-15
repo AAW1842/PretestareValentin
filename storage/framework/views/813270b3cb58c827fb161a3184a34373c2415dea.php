95b26163b7c407bc123e9fa772974ea810d493fe
Botezatu Valentin <47990519+Valent69@users.noreply.github.com>
2022-03-15 14:28:27 +0200

    Add files via upload

---
+++ storage/framework/views/813270b3cb58c827fb161a3184a34373c2415dea.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="<?php echo e(str_replace('_', '-', app()->getLocale())); ?>">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Pretestare</title>
+        <link rel="stylesheet" href="<?php echo e(asset('css/app.css')); ?>">
+        <link rel="stylesheet" href="<?php echo e(asset('css/main.css')); ?>">
+    </head>
+    <body>
+       <script src="<?php echo e(asset('js/app.js')); ?>"></script>
+       <div class="container-fluid">
+             <header style="background-image: url(imagini/back.png)">
+                 
+                 <div class="title"><h3 class="text-center">THIS IS SPECTRUM</h3></div>
+                 <div class="subtitle"><h5>Responsive Retina Ready.Sketch Support</h3></div>
+                 <button type="button" class="btn btn-light btn-lg ">Start Now</button>
+                
+                 <button type="button" class="btn btn-outline-light btn-lg ">Learn More</button>
+                
+            </header>
+            <div class="content">
+                <h3 class="text-center m-3 p-5">About Us</h3>
+                <h4 class="text-center m-3 p-3">Lorem ipsum dolor, sit amet consectetur adipisicing.</h4>
+                   <div class="container">
+                       <div class="row">
+                       <div class="row p-3 m-3">
+                            <div class="col">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum non eveniet soluta ullam temporibus praesentium magni, assumenda fugiat doloremque laboriosam libero ratione beatae amet repellat recusandae ipsam, ipsa placeat. Quod rerum cupiditate rem, minus magni excepturi consectetur vitae dolor id eaque pariatur expedita fugiat aut dolore quisquam maiores esse aspernatur amet eum nemo a. Ad!
+                            </div>
+                            <div class="col">
+                           Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat incidunt hic in tenetur numquam iusto aut neque beatae explicabo sapiente veniam ex reprehenderit, vel rerum labore iste ducimus minus. Optio libero ipsam esse voluptatibus commodi veritatis inventore facere unde. Temporibus, qui deserunt? Repellendus a illum cupiditate, ipsa nemo, iste laboriosam sit obcaecati, totam ad deleniti?
+                            </div>
+                       </div>
+                   </div>
+               
+            </div> 
+            <div class="container ">
+            <div class="cards m-3 p-3 row ">
+                <div class="card col-3 border-0 p-3" >
+                    <img class="card-img-top" src="<?php echo e(asset('imagini/img1.png')); ?>" alt="Card image cap">
+                    <div class="card-body text-center">
+                        <p class="card-text">
+                            <h3>Mihael Stud</h3> <br>
+                            <h5>Lorem, ipsum dolor.</h5>
+                        </p>
+                </div>
+            </div>
+            <div class="card col-3 border-0 p-3" >
+                <img class="card-img-top" src="<?php echo e(asset('imagini/img2.png')); ?>" alt="Card image cap">
+                <div class="card-body text-center">
+                    <p class="card-text">
+                        <h3>Mihael Stud</h3> <br>
+                        <h5>Lorem, ipsum dolor.</h5>
+                    </p>
+                </div>
+            </div>
+            <div class="card col-3 border-0 p-3" >
+                <img class="card-img-top" src="<?php echo e(asset('imagini/img3.png')); ?>" alt="Card image cap">
+                <div class="card-body text-center">
+                    <p class="card-text">
+                        <h3>Mihael Stud</h3> <br>
+                        <h5>Lorem, ipsum dolor.</h5>
+                    </p>
+                </div>
+            </div>
+            <div class="card col-3 border-0 p-3" >
+                <img class="card-img-top" src="<?php echo e(asset('imagini/img4.png')); ?>" alt="Card image cap">
+                <div class="card-body text-center">
+                    <p class="card-text">
+                        <h3>Mihael Stud</h3> <br>
+                        <h5>Lorem, ipsum dolor.</h5>
+                    </p>
+                </div>
+            </div>
+            </div>
+            
+            </div>
+           
+                
+        <div class="latest-works">
+            <h2>Latest Works</h2>
+            <div class="title-works">
+                <h5>All</h5>
+                <h5>Branding</h5>
+                <h5>Design</h5>
+                <h5>Development<h5>
+                <h5>Strategy</h5>
+            </div>
+            <div class="gallery">
+                <img src="<?php echo e(asset('imagini/gal1.png')); ?>" class="rounded " alt="...">
+                <img src="<?php echo e(asset('imagini/gal2.png')); ?>" class="rounded " alt="...">
+                <img src="<?php echo e(asset('imagini/gal3.png')); ?>" class="rounded " alt="...">
+                <img src="<?php echo e(asset('imagini/gal4.png')); ?>" class="rounded " alt="...">
+                <img src="<?php echo e(asset('imagini/gal5.png')); ?>" class="rounded " alt="...">
+                <img src="<?php echo e(asset('imagini/gal6.png')); ?>" class="rounded " alt="...">
+                <img src="<?php echo e(asset('imagini/gal7.png')); ?>" class="rounded " alt="...">
+                <img src="<?php echo e(asset('imagini/gal8.png')); ?>" class="rounded " alt="...">
+                <img src="<?php echo e(asset('imagini/gal9.png')); ?>" class="rounded " alt="...">
+            </div>
+        </div>
+
+        <div class="contacts">
+                 <h3 class="text-center m-3 p-5">GET IN TOUCH</h3>
+                <h4 class="text-center m-3 p-3">Lorem ipsum dolor, sit amet consectetur adipisicing.</h4>
+        </div>
+        <footer>
+            <div class="icons">
+            <i class="fa-brands fa-twitter"></i>
+            <i class="fa-brands fa-twitter"></i>
+            <i class="fa-brands fa-twitter"></i>
+            <h3>Botezatu Valentin</h3>
+            </div>
+        </footer>
+  
+</div>
+        </div>
+           
+       </div>
+       <link rel="stylesheet" href="<?php echo e(asset('js/main.js')); ?>">
+    </body>
+</html>
+<?php /**PATH C:\Users\PC-21402\Desktop\test\lectia8\resources\views/welcome.blade.php ENDPATH**/ ?>
